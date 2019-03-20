93dba387f0ecdaad86fdcfda01d3a32fe6de21e5
Jonas Koenemann <Jonas.Koenemann@yahoo.de>
2019-03-21 00:29:51 +0100

    Create deploy_openocl_v3.sh

---
+++ deploy_openocl_v3.sh
@@ -0,0 +1,24 @@
+
+git clone https://github.com/OpenOCL/OpenOCL.git
+
+wget https://github.com/casadi/casadi/releases/download/3.3.0/linux__casadi-matlabR2014b-v3.3.0.tar.gz
+wget https://github.com/casadi/casadi/releases/download/3.3.0/linux__casadi-octave-v3.3.0.tar.gz
+
+wget https://github.com/casadi/casadi/releases/download/3.3.0/osx__casadi-matlabR2014b-v3.3.0.tar.gz
+wget https://github.com/casadi/casadi/releases/download/3.3.0/osx__casadi-matlabR2015a-v3.3.0.tar.gz
+wget https://github.com/casadi/casadi/releases/download/3.3.0/osx__casadi-octave-v3.3.0.tar.gz
+
+wget https://github.com/casadi/casadi/releases/download/3.3.0/windows__casadi-matlabR2014b-v3.3.0.zip
+wget https://github.com/casadi/casadi/releases/download/3.3.0/windows__casadi-matlabR2016a-v3.3.0.zip
+wget https://github.com/casadi/casadi/releases/download/3.3.0/windows__casadi-octave-4.2.1-w64-v3.3.0.zip
+
+tar xvcf linux__casadi-matlabR2014b-v3.3.0.tar.gz -C linux14b
+
+cd openocl
+mkdir bin
+cd bin
+
+cp ../../linux14b/libcasadi.so . 
+
+
+
