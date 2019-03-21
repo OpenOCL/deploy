ffb2ceb22ec03adafabf681fb00dd06feecafd1b
Jonas Koenemann <Jonas.Koenemann@yahoo.de>
2019-03-21 10:59:22 +0100

    download with curl, extract zip linux

--- deploy_openocl_v3.30.sh
+++ deploy_openocl_v3.30.sh
@@ -1,25 +1,29 @@
+rm -rf ./ocl_tmp
+rm -rf ./archives_tmp
+rm -rf ./output
 
-git clone https://github.com/OpenOCL/OpenOCL.git
-git checkout tags/v3.30
+mkdir archives_tmp
 
-wget https://github.com/casadi/casadi/releases/download/3.3.0/linux__casadi-matlabR2014b-v3.3.0.tar.gz
-wget https://github.com/casadi/casadi/releases/download/3.3.0/linux__casadi-octave-v3.3.0.tar.gz
+git clone https://github.com/OpenOCL/OpenOCL.git ocl_tmp
+git checkout master
 
-wget https://github.com/casadi/casadi/releases/download/3.3.0/osx__casadi-matlabR2014b-v3.3.0.tar.gz
-wget https://github.com/casadi/casadi/releases/download/3.3.0/osx__casadi-matlabR2015a-v3.3.0.tar.gz
-wget https://github.com/casadi/casadi/releases/download/3.3.0/osx__casadi-octave-v3.3.0.tar.gz
+curl -o archives_tmp/linux-m.tar.gz -LO https://github.com/casadi/casadi/releases/download/3.3.0/linux__casadi-matlabR2014b-v3.3.0.tar.gz --progress-bar
+# wget https://github.com/casadi/casadi/releases/download/3.3.0/linux__casadi-octave-v3.3.0.tar.gz
+#
+# wget https://github.com/casadi/casadi/releases/download/3.3.0/osx__casadi-matlabR2014b-v3.3.0.tar.gz
+# wget https://github.com/casadi/casadi/releases/download/3.3.0/osx__casadi-matlabR2015a-v3.3.0.tar.gz
+# wget https://github.com/casadi/casadi/releases/download/3.3.0/osx__casadi-octave-v3.3.0.tar.gz
+#
+# wget https://github.com/casadi/casadi/releases/download/3.3.0/windows__casadi-matlabR2014b-v3.3.0.zip
+# wget https://github.com/casadi/casadi/releases/download/3.3.0/windows__casadi-matlabR2016a-v3.3.0.zip
+# wget https://github.com/casadi/casadi/releases/download/3.3.0/windows__casadi-octave-4.2.1-w64-v3.3.0.zip
 
-wget https://github.com/casadi/casadi/releases/download/3.3.0/windows__casadi-matlabR2014b-v3.3.0.zip
-wget https://github.com/casadi/casadi/releases/download/3.3.0/windows__casadi-matlabR2016a-v3.3.0.zip
-wget https://github.com/casadi/casadi/releases/download/3.3.0/windows__casadi-octave-4.2.1-w64-v3.3.0.zip
+mkdir ocl_tmp/Lib
+tar -zxf archives_tmp/linux-m.tar.gz -C ocl_tmp/Lib
 
-tar xvcf linux__casadi-matlabR2014b-v3.3.0.tar.gz -C linux14b
-
-cd openocl
-mkdir bin
-cd bin
-
-cp ../../linux14b/libcasadi.so . 
+mkdir output
+tar -zcf output/openocl_v3-30_linux_matlab.tar.gz ocl_tmp
 
 
-
+rm -rf ./ocl_tmp
+rm -rf ./archives_tmp
