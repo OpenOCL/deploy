1712289e1bb5a69a6a68dcb48a56cd045c7d710c
Jonas Koenemann <Jonas.Koenemann@yahoo.de>
2019-03-21 11:36:38 +0100

    use zip for windows binaries

--- deploy_openocl_v3-41.sh
+++ deploy_openocl_v3-41.sh
@@ -38,21 +38,28 @@ tar -zxf archives_tmp/osx_octave.tar.gz -C ocl_tmp/Lib
 tar -zcf output/openocl_v3-41_osx_octave.tar.gz -C ocl_tmp .
 rm -rf ./ocl_tmp/Lib
 
-curl -o archives_tmp/win64_2014b.tar.gz -LO https://github.com/casadi/casadi/releases/download/3.3.0/windows__casadi-matlabR2014b-v3.3.0.zip --progress-bar
-tar -zxf archives_tmp/win64_2014b.tar.gz -C ocl_tmp/Lib
-tar -zcf output/openocl_v3-41_win64_2014b.tar.gz -C ocl_tmp .
+curl -o archives_tmp/win64_matlab2014b.zip -LO https://github.com/casadi/casadi/releases/download/3.3.0/windows__casadi-matlabR2014b-v3.3.0.zip --progress-bar
+mkdir ocl_tmp/Lib
+unzip archives_tmp/win64_matlab2014b.zip -d ocl_tmp/Lib
+cd ocl_tmp/
+zip -r ../output/openocl_v3-41_win64_matlab2014b.zip *
+cd ..
 rm -rf ./ocl_tmp/Lib
 
-curl -o archives_tmp/win64_2016a.tar.gz -LO https://github.com/casadi/casadi/releases/download/3.3.0/windows__casadi-matlabR2016a-v3.3.0.zip --progress-bar
+curl -o archives_tmp/win64_matlab2016a.zip -LO https://github.com/casadi/casadi/releases/download/3.3.0/windows__casadi-matlabR2016a-v3.3.0.zip --progress-bar
 mkdir ocl_tmp/Lib
-tar -zxf archives_tmp/win64_2016a.tar.gz -C ocl_tmp/Lib
-tar -zcf output/openocl_v3-41_win64_2016a.tar.gz -C ocl_tmp .
+unzip archives_tmp/win64_matlab2016a.zip -d ocl_tmp/Lib
+cd ocl_tmp/
+zip -r ../output/openocl_v3-41_win64_matlab2016a.zip *
+cd ..
 rm -rf ./ocl_tmp/Lib
 
-curl -o archives_tmp/win64_octave_4-2-1.tar.gz -LO https://github.com/casadi/casadi/releases/download/3.3.0/windows__casadi-octave-4.2.1-w64-v3.3.0.zip --progress-bar
+curl -o archives_tmp/win64_octave_4-2-1.zip -LO https://github.com/casadi/casadi/releases/download/3.3.0/windows__casadi-octave-4.2.1-w64-v3.3.0.zip --progress-bar
 mkdir ocl_tmp/Lib
-tar -zxf archives_tmp/win64_octave_4-2-1.tar.gz -C ocl_tmp/Lib
-tar -zcf output/openocl_v3-41_win64_octave_4-2-1.tar.gz -C ocl_tmp .
+unzip archives_tmp/win64_octave_4-2-1.zip -d ocl_tmp/Lib
+cd ocl_tmp/
+zip -r ../output/openocl_v3-41_win64_octave_4-2-1.zip *
+cd ..
 rm -rf ./ocl_tmp/Lib
 
 rm -rf ./ocl_tmp
