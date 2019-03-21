0dff00f8854c5bc5faa38b4ca104d1d126f5bb33
Jonas Koenemann <Jonas.Koenemann@yahoo.de>
2019-03-21 11:45:49 +0100

    version names

--- deploy_openocl_v3-41.sh
+++ deploy_openocl_v3-41.sh
@@ -8,16 +8,16 @@ mkdir output
 git clone https://github.com/OpenOCL/OpenOCL.git ocl_tmp
 git checkout master
 
-curl -o archives_tmp/linux_matlab.tar.gz -LO https://github.com/casadi/casadi/releases/download/3.3.0/linux__casadi-matlabR2014b-v3.3.0.tar.gz --progress-bar
+curl -o archives_tmp/linux_matlab2014b.tar.gz -LO https://github.com/casadi/casadi/releases/download/3.3.0/linux__casadi-matlabR2014b-v3.3.0.tar.gz --progress-bar
 mkdir ocl_tmp/Lib
-tar -zxf archives_tmp/linux_matlab.tar.gz -C ocl_tmp/Lib
-tar -zcf output/openocl_v3-41_linux_matlab.tar.gz -C ocl_tmp .
+tar -zxf archives_tmp/linux_matlab2014b.tar.gz -C ocl_tmp/Lib
+tar -zcf output/openocl_v3-41_linux_matlab2014b.tar.gz -C ocl_tmp .
 rm -rf ./ocl_tmp/Lib
 
-curl -o archives_tmp/linux_octave.tar.gz -LO https://github.com/casadi/casadi/releases/download/3.3.0/linux__casadi-octave-v3.3.0.tar.gz --progress-bar
+curl -o archives_tmp/linux_octave_4-2-1.tar.gz -LO https://github.com/casadi/casadi/releases/download/3.3.0/linux__casadi-octave-v3.3.0.tar.gz --progress-bar
 mkdir ocl_tmp/Lib
-tar -zxf archives_tmp/linux_octave.tar.gz -C ocl_tmp/Lib
-tar -zcf output/openocl_v3-41_linux_octave.tar.gz -C ocl_tmp .
+tar -zxf archives_tmp/linux_octave_4-2-1.tar.gz -C ocl_tmp/Lib
+tar -zcf output/openocl_v3-41_linux_octave_4-2-1.tar.gz -C ocl_tmp .
 rm -rf ./ocl_tmp/Lib
 
 curl -o archives_tmp/osx_matlab2014b.tar.gz -LO https://github.com/casadi/casadi/releases/download/3.3.0/osx__casadi-matlabR2014b-v3.3.0.tar.gz --progress-bar
@@ -26,39 +26,39 @@ tar -zxf archives_tmp/osx_matlab2014b.tar.gz -C ocl_tmp/Lib
 tar -zcf output/openocl_v3-41_osx_matlab2014b.tar.gz -C ocl_tmp .
 rm -rf ./ocl_tmp/Lib
 
-curl -o archives_tmp/osx_matlab.tar.gz -LO https://github.com/casadi/casadi/releases/download/3.3.0/osx__casadi-matlabR2015a-v3.3.0.tar.gz --progress-bar
+curl -o archives_tmp/osx_matlab2015a.tar.gz -LO https://github.com/casadi/casadi/releases/download/3.3.0/osx__casadi-matlabR2015a-v3.3.0.tar.gz --progress-bar
 mkdir ocl_tmp/Lib
-tar -zxf archives_tmp/osx_matlab.tar.gz -C ocl_tmp/Lib
-tar -zcf output/openocl_v3-41_osx_matlab.tar.gz -C ocl_tmp .
+tar -zxf archives_tmp/osx_matlab2015a.tar.gz -C ocl_tmp/Lib
+tar -zcf output/openocl_v3-41_osx_matlab2015a.tar.gz -C ocl_tmp .
 rm -rf ./ocl_tmp/Lib
 
-curl -o archives_tmp/osx_octave.tar.gz -LO https://github.com/casadi/casadi/releases/download/3.3.0/osx__casadi-octave-v3.3.0.tar.gz --progress-bar
+curl -o archives_tmp/osx_octave_4-2-1.tar.gz -LO https://github.com/casadi/casadi/releases/download/3.3.0/osx__casadi-octave-v3.3.0.tar.gz --progress-bar
 mkdir ocl_tmp/Lib
-tar -zxf archives_tmp/osx_octave.tar.gz -C ocl_tmp/Lib
-tar -zcf output/openocl_v3-41_osx_octave.tar.gz -C ocl_tmp .
+tar -zxf archives_tmp/osx_octave_4-2-1.tar.gz -C ocl_tmp/Lib
+tar -zcf output/openocl_v3-41_osx_octave_4-2-1.tar.gz -C ocl_tmp .
 rm -rf ./ocl_tmp/Lib
 
 curl -o archives_tmp/win64_matlab2014b.zip -LO https://github.com/casadi/casadi/releases/download/3.3.0/windows__casadi-matlabR2014b-v3.3.0.zip --progress-bar
 mkdir ocl_tmp/Lib
-unzip archives_tmp/win64_matlab2014b.zip -d ocl_tmp/Lib
+unzip -q archives_tmp/win64_matlab2014b.zip -d ocl_tmp/Lib
 cd ocl_tmp/
-zip -r ../output/openocl_v3-41_win64_matlab2014b.zip *
+zip -r -q -X ../output/openocl_v3-41_win64_matlab2014b.zip *
 cd ..
 rm -rf ./ocl_tmp/Lib
 
 curl -o archives_tmp/win64_matlab2016a.zip -LO https://github.com/casadi/casadi/releases/download/3.3.0/windows__casadi-matlabR2016a-v3.3.0.zip --progress-bar
 mkdir ocl_tmp/Lib
-unzip archives_tmp/win64_matlab2016a.zip -d ocl_tmp/Lib
+unzip -q archives_tmp/win64_matlab2016a.zip -d ocl_tmp/Lib
 cd ocl_tmp/
-zip -r ../output/openocl_v3-41_win64_matlab2016a.zip *
+zip -r -q -X ../output/openocl_v3-41_win64_matlab2016a.zip *
 cd ..
 rm -rf ./ocl_tmp/Lib
 
 curl -o archives_tmp/win64_octave_4-2-1.zip -LO https://github.com/casadi/casadi/releases/download/3.3.0/windows__casadi-octave-4.2.1-w64-v3.3.0.zip --progress-bar
 mkdir ocl_tmp/Lib
-unzip archives_tmp/win64_octave_4-2-1.zip -d ocl_tmp/Lib
+unzip -q archives_tmp/win64_octave_4-2-1.zip -d ocl_tmp/Lib
 cd ocl_tmp/
-zip -r ../output/openocl_v3-41_win64_octave_4-2-1.zip *
+zip -r -q -X ../output/openocl_v3-41_win64_octave_4-2-1.zip *
 cd ..
 rm -rf ./ocl_tmp/Lib
 
