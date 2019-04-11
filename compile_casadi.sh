f0a957631c1424cb64951a8cb5b57da2f6753d0e
Jonas Koenemann <Jonas.Koenemann@yahoo.de>
2019-04-11 14:34:07 +0200

    compilation of casadi linux w octave matlab

--- compile_casadi.sh
+++ compile_casadi.sh
@@ -1,15 +1,25 @@
+sudo apt-get install liboctave-dev -y
+
+rm -rf build
+mkdir build
+cd build
+
 # get casadi source code
-git clone https://github.com/casadi/casadi.git
+git clone https://github.com/casadi/casadi.git --depth=1 casadi
 
 # build casadi for linux
 cd casadi
 mkdir build
 cd build
-export CC = clang++
-cmake -DWITH_OPENMP=ON -DWITH_EXTRA_WARNINGS=ON -DWITH_EXTRA_CHECKS=ON -DWITH_PROFILING=ON -DWITH_DOC=ON -DWITH_EXAMPLES=ON -DWITH_COVERAGE=ON -DWITH_EXTRA_WARNINGS=ON -DWITH_JSON=ON -DWITH_BLASFEO=ON -DWITH_BUILD_BLASFEO=ON -DWITH_HPMPC=ON -DWITH_BUILD_HPMPC=ON ..
+
+# export CXXFLAGS=x86_64-pc-linux
+export CC=clang
+export CXX=clang++
+
+cmake -DWITH_SLICOT=OFF -DWITH_DEEPBIND=ON -DWITH_OCTAVE=ON -DWITH_OCTAVE_IMPORT=ON -DWITH_OPENMP=ON -DWITH_EXTRA_WARNINGS=ON -DWITH_EXTRA_CHECKS=ON -DWITH_EXTRA_WARNINGS=ON -DWITH_JSON=ON ..
+make -j4
 
 # get OpenOCL source code
-git clone https://github.com/OpenOCL/OpenOCL.git
+# git clone https://github.com/OpenOCL/OpenOCL.git
 
 # create linux package
-
