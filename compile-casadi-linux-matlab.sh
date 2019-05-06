93b0a648fa36cc49d0ecea34e75cb2f25abace89
Jonas C K <Jonas.Koenemann@yahoo.de>
2019-05-06 14:36:51 +0200

    Update compile-casadi-linux-matlab.sh

--- compile-casadi-linux-matlab.sh
+++ compile-casadi-linux-matlab.sh
@@ -1,3 +1,4 @@
+# ipopt
 VERSION=3.12.3
 wget http://www.coin-or.org/download/source/Ipopt/Ipopt-$VERSION.tgz
 tar -xvf Ipopt-$VERSION.tgz
@@ -25,11 +26,46 @@ make install
 
 tar -zcvf ipopt.tar.gz -C $HOME/ipopt-install .
 
+# swig matlab
+sudo apt-get install -y libpcre3-dev automake yodl
+cd restricted 
+git clone https://github.com/jaeandersson/swig.git
+cd swig 
+git checkout $BAKEVERSION 
+./autogen.sh 
+./configure --prefix=$mypwd/swig-matlab-install 
+make 
+make install
+cd ..
+cd ..
 
+# setup compiler
+sudo apt-get update -qq
+sudo apt-get install -y binutils gcc-4.7 g++-4.7 gfortran-4.7 git cmake liblapack-dev ipython python-dev python-numpy python-scipy python-matplotlib libmumps-seq-dev libblas-dev liblapack-dev libxml2-dev
+export CC=gcc-4.7
+export CXX=g++-4.7
+export FC=gfortran-4.7
+sudo apt-get install fakeroot rpm alien
+
+export SWIG_HOME=$HOME/build/swig_matlab
+export PATH=$SWIG_HOME/bin:$SWIG_HOME/share:$PATH
 
 export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:$HOME/ipopt-install/lib/pkgconfig
 cd $HOME
 ln -s  $HOME/build/ipopt ipopt-install
 cd ..
-export casadi_build_flags="$casadi_build_flags -DWITH_IPOPT=ON"    
 
+# compile
+cd /home/travis/build/
+testbot/recipes/fetch.sh matlab$MATLABRELEASE.tar.gz
+mkdir matlab
+tar -xf matlab$MATLABRELEASE.tar.gz -C matlab
+cd ..
+cd casadi
+mkdir build
+mkdir -p build/swig/matlab/ 
+touch build/swig/matlab/SwigGet.m
+cd build
+cmake -DWITH_IPOPT=ON -DWITH_OPENMP=OFF -DWITH_SELFCONTAINED=ON -DCMAKE_INSTALL_PREFIX=$MATLABPATH -DWITH_DEEPBIND=ON -DWITH_MATLAB=ON  -DWITH_DOC=ON -DWITH_EXAMPLES=ON -DWITH_EXTRA_WARNINGS=ON ..
+make VERBOSE=1
+make install VERBOSE=1
