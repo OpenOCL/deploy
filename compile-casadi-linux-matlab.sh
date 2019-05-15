7c2e2b902dfd7c86506da610f7208ebc27104657
Jonas Koenemann <Jonas.Koenemann@yahoo.de>
2019-05-15 08:55:20 +0200

    check for completed steps

--- compile-casadi-linux-matlab.sh
+++ compile-casadi-linux-matlab.sh
@@ -5,89 +5,110 @@ if [ -z "${MATLAB_ROOT}" ]; then
    exit 1
 fi
 
-sudo apt-get update -qq
-sudo apt-get install p7zip-full -y
-sudo apt-get install bison -y
-sudo apt-get install -y binutils gcc g++ gfortran git cmake liblapack-dev ipython
-sudo apt-get install -y python-dev python-numpy python-scipy python-matplotlib libmumps-seq-dev
-sudo apt-get install -y libblas-dev liblapack-dev libxml2-dev
-sudo apt-get install -y fakeroot rpm alien
+if [ ! -e "$HOME/build-casadi-linux-matlab" ]; then
+  mkdir $HOME/build-casadi-linux-matlab
+fi
 
-rm -rf $HOME/build-casadi-linux-matlab
-mkdir $HOME/build-casadi-linux-matlab
 cd $HOME/build-casadi-linux-matlab
 
-# get ipopt
-wget http://www.coin-or.org/download/source/Ipopt/Ipopt-3.12.3.tgz
-tar -xf Ipopt-3.12.3.tgz
-cd Ipopt-3.12.3
-cd ThirdParty
 
-cd Metis
-./get.Metis
-cd ..
+if [ ! -f "APT_COMPLETE" ]; then
 
-cd Blas
-./get.Blas
-cd ..
+  sudo apt-get update -qq
+  sudo apt-get install p7zip-full -y
+  sudo apt-get install bison -y
+  sudo apt-get install -y binutils gcc g++ gfortran git cmake liblapack-dev ipython
+  sudo apt-get install -y python-dev python-numpy python-scipy python-matplotlib libmumps-seq-dev
+  sudo apt-get install -y libblas-dev liblapack-dev libxml2-dev
+  sudo apt-get install -y fakeroot rpm alien
 
-cd Lapack
-./get.Lapack
-cd ..
+  touch APT_COMPLETE
 
-cd Mumps
-./get.Mumps
-cd ..
+fi # APT_COMPLETE
 
-cd ..
-mkdir build
-cd build
-mkdir $HOME/build-casadi-linux-matlab/ipopt-install
-../configure coin_skip_warn_cxxflags=yes --prefix=$HOME/build-casadi-linux-matlab/ipopt-install \
-    --disable-shared ADD_FFLAGS=-fPIC ADD_CFLAGS=-fPIC ADD_CXXFLAGS=-fPIC --with-blas=BUILD \
-    --with-lapack=BUILD --with-mumps=BUILD --with-metis=BUILD --without-hsl --without-asl
-make -j4
-make install
-cd ..
-cd ..
+if [ ! -f "IPOPT_COMPLETE" ]; then
 
-rm Ipopt-3.12.3.tgz
-rm -rf Ipopt-3.12.3
+  wget http://www.coin-or.org/download/source/Ipopt/Ipopt-3.12.3.tgz
+  tar -xf Ipopt-3.12.3.tgz
+  cd Ipopt-3.12.3
+  cd ThirdParty
 
-# swig matlab
-sudo apt-get install -y libpcre3-dev automake yodl
-git clone --depth=1 https://github.com/jaeandersson/swig.git
-cd swig
-./autogen.sh
-./configure --prefix=$HOME/build-casadi-linux-matlab/swig-install
-make -j4
-make install
-cd ..
+  cd Metis
+  ./get.Metis
+  cd ..
 
-rm -rf swig
+  cd Blas
+  ./get.Blas
+  cd ..
 
-# setup compiler
-export SWIG_HOME=${HOME}/build-casadi-linux-matlab/swig-install
-export PATH=${SWIG_HOME}/bin:${SWIG_HOME}/share:${PATH}
-export PKG_CONFIG_PATH=${PKG_CONFIG_PATH}:${HOME}/build-casadi-linux-matlab/ipopt-install/lib/pkgconfig
+  cd Lapack
+  ./get.Lapack
+  cd ..
 
-# compile
-git clone https://github.com/casadi/casadi.git --depth=1
-cd casadi
-mkdir build
-cd build
-cmake -DCMAKE_INSTALL_PREFIX=$HOME/build-casadi-linux-matlab/casadi-install -DWITH_OSQP=OFF \
-    -DWITH_THREAD_MINGW=OFF -DWITH_THREAD=ON -DWITH_AMPL=OFF -DCMAKE_BUILD_TYPE=Release \
-    -DWITH_SO_VERSION=OFF -DWITH_NO_QPOASES_BANNER=ON -DWITH_COMMON=ON -DWITH_HPMPC=OFF \
-    -DWITH_BUILD_HPMPC=OFF -DWITH_BLASFEO=OFF -DWITH_BUILD_BLASFEO=OFF -DINSTALL_INTERNAL_HEADERS=ON \
-    -DWITH_IPOPT=ON -DWITH_OPENMP=ON -DWITH_SELFCONTAINED=ON -DWITH_DEEPBIND=ON -DWITH_MATLAB=ON \
-    -DWITH_DOC=OFF -DWITH_EXAMPLES=OFF -DWITH_EXTRA_WARNINGS=ON ..
-make -j4
-make install
+  cd Mumps
+  ./get.Mumps
+  cd ..
 
-cd ..
-cd ..
-rm -r casadi
+  cd ..
+  mkdir build
+  cd build
+  mkdir $HOME/build-casadi-linux-matlab/ipopt-install
+  ../configure coin_skip_warn_cxxflags=yes --prefix=$HOME/build-casadi-linux-matlab/ipopt-install \
+      --disable-shared ADD_FFLAGS=-fPIC ADD_CFLAGS=-fPIC ADD_CXXFLAGS=-fPIC --with-blas=BUILD \
+      --with-lapack=BUILD --with-mumps=BUILD --with-metis=BUILD --without-hsl --without-asl
+  make -j4
+  make install
+  cd ..
+  cd ..
+
+  rm Ipopt-3.12.3.tgz
+  rm -rf Ipopt-3.12.3
+  touch IPOPT_COMPLETE
+
+fi # IPOPT_COMPLETE
+
+if [ ! -f "SWIG_COMPLETE" ]; then
+
+  sudo apt-get install -y libpcre3-dev automake yodl
+  git clone --depth=1 https://github.com/jaeandersson/swig.git
+  cd swig
+  ./autogen.sh
+  ./configure --prefix=$HOME/build-casadi-linux-matlab/swig-install
+  make -j4
+  make install
+  cd ..
+
+  rm -rf swig
+  touch SWIG_COMPLETE
+
+fi # SWIG_COMPLETE
+
+if [ ! -f "CASADI_COMPLETE" ]; then
+
+  export SWIG_HOME=${HOME}/build-casadi-linux-matlab/swig-install
+  export PATH=${SWIG_HOME}/bin:${SWIG_HOME}/share:${PATH}
+  export PKG_CONFIG_PATH=${PKG_CONFIG_PATH}:${HOME}/build-casadi-linux-matlab/ipopt-install/lib/pkgconfig
+
+  git clone https://github.com/casadi/casadi.git --depth=1
+  cd casadi
+  mkdir build
+  cd build
+  cmake -DCMAKE_INSTALL_PREFIX=$HOME/build-casadi-linux-matlab/casadi-install -DWITH_OSQP=OFF \
+      -DWITH_THREAD_MINGW=OFF -DWITH_THREAD=ON -DWITH_AMPL=OFF -DCMAKE_BUILD_TYPE=Release \
+      -DWITH_SO_VERSION=OFF -DWITH_NO_QPOASES_BANNER=ON -DWITH_COMMON=ON -DWITH_HPMPC=OFF \
+      -DWITH_BUILD_HPMPC=OFF -DWITH_BLASFEO=OFF -DWITH_BUILD_BLASFEO=OFF -DINSTALL_INTERNAL_HEADERS=ON \
+      -DWITH_IPOPT=ON -DWITH_OPENMP=ON -DWITH_SELFCONTAINED=ON -DWITH_DEEPBIND=ON -DWITH_MATLAB=ON \
+      -DWITH_DOC=OFF -DWITH_EXAMPLES=OFF -DWITH_EXTRA_WARNINGS=ON ..
+  make -j4
+  make install
+
+  cd ..
+  cd ..
+  rm -r casadi
+
+  touch CASADI_COMPLETE
+
+fi # CASADI_COMPLETE
 
 export datestr=$(date +"%Y%m%d%H%M%")
 zip -r casadi-3.4.5-linux-matlab-ipopt-minimal-${datestr}.zip casadi-install
--- compile-casadi-win-matlab.sh
+++ compile-casadi-win-matlab.sh
@@ -5,107 +5,131 @@ if [ -z "${MATLAB_ROOT}" ]; then
    exit 1
 fi
 
-sudo apt-get update -qq
-sudo apt-get install p7zip-full -y
-sudo apt-get install bison -y
-sudo apt-get install -y binutils gcc g++ gfortran git cmake liblapack-dev ipython
-sudo apt-get install -y python-dev python-numpy python-scipy python-matplotlib
-sudo apt-get install -y libmumps-seq-dev libblas-dev liblapack-dev libxml2-dev
-sudo apt-get install -y fakeroot rpm alien
-sudo apt-get install -y libpcre3-dev automake yodl
-sudo apt-get install -y dpkg
-sudo apt-get install -y mingw-w64 g++-mingw-w64 gcc-mingw-w64 gfortran-mingw-w64 mingw-w64-tools
+if [ ! -e "$HOME/build-casadi-win-matlab" ]; then
+  mkdir $HOME/build-casadi-win-matlab
+fi
 
-rm -rf $HOME/build-casadi-win-matlab
-mkdir $HOME/build-casadi-win-matlab
+cd $HOME/build-casadi-win-matlab
+
+if [ ! -f "APT_COMPLETE" ]; then
+
+  sudo apt-get update -qq
+  sudo apt-get install p7zip-full -y
+  sudo apt-get install bison -y
+  sudo apt-get install -y binutils gcc g++ gfortran git cmake liblapack-dev ipython
+  sudo apt-get install -y python-dev python-numpy python-scipy python-matplotlib
+  sudo apt-get install -y libmumps-seq-dev libblas-dev liblapack-dev libxml2-dev
+  sudo apt-get install -y fakeroot rpm alien
+  sudo apt-get install -y libpcre3-dev automake yodl
+  sudo apt-get install -y dpkg
+  sudo apt-get install -y mingw-w64 g++-mingw-w64 gcc-mingw-w64 gfortran-mingw-w64 mingw-w64-tools
+
+  touch APT_COMPLETE
+
+fi # APT_COMPLETE
+
 
 cp CMakeListsWinMatlab.txt $HOME/build-casadi-win-matlab
 cp toolchain-casadi.cmake $HOME/build-casadi-win-matlab
 
-cd $HOME/build-casadi-win-matlab
 
-# swig matlab w pcre
-git clone https://github.com/jaeandersson/swig.git --depth=1
-cd swig
-wget ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre/pcre-8.42.tar.gz
-sh Tools/pcre-build.sh --host=x86_64 CC=x86_64-w64-mingw32-gcc CXX=x86_64-w64-mingw32-g++
-./autogen.sh
-./configure --prefix=$HOME/build-casadi-win-matlab/swig-install \
-    --host=x86_64 CC=x86_64-w64-mingw32-gcc CXX=x86_64-w64-mingw32-g++
-make -j4
-make install
+if [ ! -f "SWIG_COMPLETE" ]; then
 
-cd ..
-rm -rf swig
+  # swig matlab w pcre
+  git clone https://github.com/jaeandersson/swig.git --depth=1
+  cd swig
+  wget ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre/pcre-8.42.tar.gz
+  sh Tools/pcre-build.sh --host=x86_64 CC=x86_64-w64-mingw32-gcc CXX=x86_64-w64-mingw32-g++
+  ./autogen.sh
+  ./configure --prefix=$HOME/build-casadi-win-matlab/swig-install \
+      --host=x86_64 CC=x86_64-w64-mingw32-gcc CXX=x86_64-w64-mingw32-g++
+  make -j4
+  make install
 
-# get ipopt
-wget http://www.coin-or.org/download/source/Ipopt/Ipopt-3.12.3.tgz
-tar -xf Ipopt-3.12.3.tgz
-cd Ipopt-3.12.3
-cd ThirdParty
+  cd ..
+  rm -rf swig
 
-cd Metis
-./get.Metis
-cd ..
+  touch SWIG_COMPLETE
 
-cd Blas
-./get.Blas
-cd ..
+fi # SWIG_COMPLETE
 
-cd Lapack
-./get.Lapack
-cd ..
+if [ ! -f "IPOPT_COMPLETE" ]; then
 
-cd Mumps
-./get.Mumps
-cd ..
+  wget http://www.coin-or.org/download/source/Ipopt/Ipopt-3.12.3.tgz
+  tar -xf Ipopt-3.12.3.tgz
+  cd Ipopt-3.12.3
+  cd ThirdParty
 
-cd ..
+  cd Metis
+  ./get.Metis
+  cd ..
 
-mkdir build
-cd build
-mkdir $HOME/build-casadi-win-matlab/ipopt-install
-../configure --prefix=$HOME/build-casadi-win-matlab/ipopt-install --host x86_64-w64-mingw32 \
-    --enable-dependency-linking --build mingw32 --disable-shared ADD_FFLAGS=-fPIC ADD_CFLAGS=-fPIC ADD_CXXFLAGS=-fPIC \
-    --with-blas=BUILD --with-lapack=BUILD --with-mumps=BUILD --with-metis=BUILD --without-hsl --without-asl
-make -j4
-make install
-cd ..
-cd ..
+  cd Blas
+  ./get.Blas
+  cd ..
 
-rm Ipopt-3.12.3.tgz
-rm -rf Ipopt-3.12.3
+  cd Lapack
+  ./get.Lapack
+  cd ..
 
-# setup compiler
-export SWIG_HOME=${HOME}/build-casadi-win-matlab/swig-install
-export PATH=${SWIG_HOME}/bin:${SWIG_HOME}/share:${PATH}
-export PKG_CONFIG_PATH=${PKG_CONFIG_PATH}:${HOME}/build-casadi-win-matlab/ipopt-install/lib/pkgconfig
+  cd Mumps
+  ./get.Mumps
+  cd ..
 
-export LDFLAGS=-static-libstdc++
+  cd ..
 
-# compile
-git clone --branch 3.4.5 https://github.com/casadi/casadi.git --depth=1
+  mkdir build
+  cd build
+  mkdir $HOME/build-casadi-win-matlab/ipopt-install
+  ../configure --prefix=$HOME/build-casadi-win-matlab/ipopt-install --host x86_64-w64-mingw32 \
+      --enable-dependency-linking --build mingw32 --disable-shared ADD_FFLAGS=-fPIC ADD_CFLAGS=-fPIC ADD_CXXFLAGS=-fPIC \
+      --with-blas=BUILD --with-lapack=BUILD --with-mumps=BUILD --with-metis=BUILD --without-hsl --without-asl
+  make -j4
+  make install
+  cd ..
+  cd ..
 
-mv casadi/swig/matlab/CMakeLists.txt casadi/swig/matlab/CMakeLists_bkp.txt
-cp CMakeListsWinMatlab.txt casadi/swig/matlab/CMakeLists.txt
+  rm Ipopt-3.12.3.tgz
+  rm -rf Ipopt-3.12.3
 
-cd casadi
-mkdir build
-cp ../toolchain-casadi.cmake .
-cd build
-cmake -DCMAKE_INSTALL_PREFIX=$HOME/build-casadi-win-matlab/casadi-install -DMATLAB_EXTRA_CXXFLAGS=\"-D__STDC_UTF_16__\"  \
-    -DCMAKE_TOOLCHAIN_FILE=../toolchain-casadi.cmake -DWITH_OSQP=OFF -DWITH_THREAD_MINGW=OFF \
-    -DWITH_THREAD=ON -DWITH_AMPL=OFF -DCMAKE_BUILD_TYPE=Release -DWITH_SO_VERSION=OFF \
-    -DWITH_NO_QPOASES_BANNER=ON -DWITH_COMMON=ON -DWITH_HPMPC=OFF -DWITH_BUILD_HPMPC=OFF \
-    -DWITH_BLASFEO=OFF -DWITH_BUILD_BLASFEO=OFF -DINSTALL_INTERNAL_HEADERS=ON -DWITH_IPOPT=ON \
-    -DWITH_OPENMP=ON -DWITH_SELFCONTAINED=ON -DWITH_DEEPBIND=ON -DWITH_MATLAB=ON -DWITH_DOC=OFF \
-    -DWITH_EXAMPLES=OFF -DWITH_EXTRA_WARNINGS=ON -DMATLAB_ROOT=${MATLAB_ROOT} ..
-make -j4
-make install
+  touch IPOPT_COMPLETE
 
-cd ..
-cd ..
-rm -r casadi
+fi # IPOPT_COMPLETE
+
+if [ ! -f "CASADI_COMPLETE" ]; then
+
+  export SWIG_HOME=${HOME}/build-casadi-win-matlab/swig-install
+  export PATH=${SWIG_HOME}/bin:${SWIG_HOME}/share:${PATH}
+  export PKG_CONFIG_PATH=${PKG_CONFIG_PATH}:${HOME}/build-casadi-win-matlab/ipopt-install/lib/pkgconfig
+
+  export LDFLAGS=-static-libstdc++
+
+  git clone --branch 3.4.5 https://github.com/casadi/casadi.git --depth=1
+
+  mv casadi/swig/matlab/CMakeLists.txt casadi/swig/matlab/CMakeLists_bkp.txt
+  cp CMakeListsWinMatlab.txt casadi/swig/matlab/CMakeLists.txt
+
+  cd casadi
+  mkdir build
+  cp ../toolchain-casadi.cmake .
+  cd build
+  cmake -DCMAKE_INSTALL_PREFIX=$HOME/build-casadi-win-matlab/casadi-install -DMATLAB_EXTRA_CXXFLAGS=\"-D__STDC_UTF_16__\"  \
+      -DCMAKE_TOOLCHAIN_FILE=../toolchain-casadi.cmake -DWITH_OSQP=OFF -DWITH_THREAD_MINGW=OFF \
+      -DWITH_THREAD=ON -DWITH_AMPL=OFF -DCMAKE_BUILD_TYPE=Release -DWITH_SO_VERSION=OFF \
+      -DWITH_NO_QPOASES_BANNER=ON -DWITH_COMMON=ON -DWITH_HPMPC=OFF -DWITH_BUILD_HPMPC=OFF \
+      -DWITH_BLASFEO=OFF -DWITH_BUILD_BLASFEO=OFF -DINSTALL_INTERNAL_HEADERS=ON -DWITH_IPOPT=ON \
+      -DWITH_OPENMP=ON -DWITH_SELFCONTAINED=ON -DWITH_DEEPBIND=ON -DWITH_MATLAB=ON -DWITH_DOC=OFF \
+      -DWITH_EXAMPLES=OFF -DWITH_EXTRA_WARNINGS=ON -DMATLAB_ROOT=${MATLAB_ROOT} ..
+  make -j4
+  make install
+
+  cd ..
+  cd ..
+  rm -r casadi
+
+  touch CASADI_COMPLETE
+fi # CASADI_COMPLETE
+
 
 export datestr=$(date +"%Y%m%d%H%M%")
 zip -r casadi-3.4.5-win-matlab-ipopt-minimal-${datestr}.zip casadi-install
