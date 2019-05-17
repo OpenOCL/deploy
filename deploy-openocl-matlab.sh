8e15910f45a89c7e561cc0107cd58595502730d8
Jonas Koenemann <Jonas.Koenemann@yahoo.de>
2019-05-17 11:32:50 +0200

    in folder deployment

--- deploy-openocl-matlab.sh
+++ deploy-openocl-matlab.sh
@@ -1,25 +1,20 @@
+rm -rf export/OpenOCL
 OCL_VERSION=4.29
 DATESTR=$(date +"%Y\/%m\/%d")
 DATEVERSION="$OCL_VERSION, $DATESTR"
 
-mkdir $HOME/openocl-deploy-pure-$OCL_VERSION
-
-# set date and version in readme marked by $dateversion$
-sed "s/#dateversion#/$DATEVERSION/g" README > $HOME/openocl-deploy-pure-$OCL_VERSION/README
-sed "s/#version#/$OCL_VERSION/g" version.m > $HOME/openocl-deploy-pure-$OCL_VERSION/version.m
-
-cd $HOME/openocl-deploy-pure-$OCL_VERSION
-
 git clone https://github.com/OpenOCL/OpenOCL.git openocl_repo
 cd openocl_repo
-git checkout-index -a -f --prefix=$HOME/openocl-deploy-pure-$OCL_VERSION/openocl/
+git checkout-index -a -f --prefix=../export/openocl/
 cd ..
 rm -rf openocl_repo
-rm openocl/.gitignore
-rm openocl/README.md
-mv README ./openocl/README
-mv version.m ./openocl/+ocl/
+rm export/OpenOCL/.gitignore
+rm export/OpenOCL/README.md
 
-cd openocl
+# set date and version in readme marked by $dateversion$
+sed "s/#dateversion#/$DATEVERSION/g" README > export/OpenOCL/README
+sed "s/#version#/$OCL_VERSION/g" version.m > export/OpenOCL/+ocl/version.m
+
+cd OpenOCL
 zip -r -q ../OpenOCL-v${OCL_VERSION}.zip .
 cd ..
